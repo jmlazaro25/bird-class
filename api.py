6d5819e430f011c18f8258cb4ddfebc5a31d4ae4
jmlazaro <juan.lazaro2599@gmail.com>
2023-10-21 17:41:32 -0700

    enable img resizing, interp, cropping, & check file type and size

--- api.py
+++ api.py
@@ -2,16 +2,15 @@ from json import load
 from fastapi import FastAPI
 from tensorflow.keras.models import load_model
 from tensorflow.keras.utils import load_img, img_to_array
-#from PIL import Image
 from io import BytesIO
 from numpy import array as nparray
 
-from fastapi import FastAPI, UploadFile
+from fastapi import FastAPI, UploadFile, HTTPException
 
 
 MODEL_DIR = 'model'
 MODEL = load_model(MODEL_DIR)
-TARGET_SIZE = (244, 244)
+TARGET_SIZE = MODEL.input_shape[1:3]
 with open('classes.json') as f:
     CLASSES = load(f)
 
@@ -20,8 +19,9 @@ def load_image(image_io):
         img_to_array(
             load_img(
                 image_io,
-                #target_size=TARGET_SIZE,
-                #interpolation='bilinear'
+                target_size=TARGET_SIZE,
+                interpolation='bilinear',
+                keep_aspect_ratio=True
             )
         )
     ])
@@ -31,6 +31,15 @@ app = FastAPI()
 
 @app.post("/predict-image")
 async def predict_image(file: UploadFile):
+
+    allowed_content_types = ["image/jpeg", "image/png"]
+    if file.content_type not in allowed_content_types:
+        raise HTTPException(status_code=415, detail="Unsupported file format")
+
+    max_file_size = 1024 * 1024  # 1 MiB in bytes
+    if file.file.tell() > max_file_size:
+        raise HTTPException(status_code=413, detail="File size exceeds 1 MiB")
+
     file_bytes = await file.read()
     img = load_image(BytesIO(file_bytes))
     probs = MODEL.predict(img)[0]
